4fa42cfc1046ded8e93ffbd23c31aef774f5476a
Lukáš Kurčík <lukas.kurcik@gmail.com>
2020-06-03 18:35:43 +0200

    Database connection

--- Rubybooru/Startup.cs
+++ Rubybooru/Startup.cs
@@ -5,17 +5,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Rubybooru.Data;
 
 namespace Rubybooru
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<RubybooruDbContext>(options =>
+            {
+                options.UseMySQL(_configuration.GetConnectionString("RubybooruDb"));
+            });
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,8 +48,10 @@ namespace Rubybooru
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    context.Response.ContentType = "text/*; charset=utf-8";
+                    await context.Response.WriteAsync("がんばルビィ");
                 });
+                endpoints.MapControllers();
             });
         }
     }
