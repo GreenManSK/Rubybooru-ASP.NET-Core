4b8d1ff048892c6e3e3d6e548bab61dcc9f48e4d
Lukáš Kurčík <lukas.kurcik@gmail.com>
2020-06-03 22:54:55 +0200

    SqlImageTag implementation

---
+++ Rubybooru.Data/Sql/SqlImageTagData.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using Rubybooru.Core;
+using Rubybooru.Data.Interfaces;
+
+namespace Rubybooru.Data.Sql
+{
+    public class SqlImageTagData : IImageTagData
+    {
+        private readonly RubybooruDbContext _db;
+
+        public SqlImageTagData(RubybooruDbContext db)
+        {
+            _db = db;
+        }
+
+        public ImageTag Add(ImageTag tag)
+        {
+            _db.Add(tag);
+            return tag;
+        }
+
+        public ImageTag Update(ImageTag tag)
+        {
+            var entity = _db.ImageTag.Attach(tag);
+            entity.State = EntityState.Modified;
+            return tag;
+        }
+
+        public ImageTag Delete(int id)
+        {
+            var tag = _db.ImageTag.Find(id);
+
+            if (tag != null)
+            {
+                _db.ImageTag.Remove(tag);
+            }
+
+            return tag;
+        }
+
+        public int Commit()
+        {
+            return _db.SaveChanges();
+        }
+    }
+}
