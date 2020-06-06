173cd99d5acec002e8f28de52f7ef25d22d6b01f
Lukáš Kurčík <lukas.kurcik@gmail.com>
2020-06-06 15:30:17 +0200

    Image api for angular

--- Rubybooru.Angular/src/app/app.component.ts
+++ Rubybooru.Angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ImageApiService } from './services/image-api/image-api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rubybooru';
+
+  constructor(imageApi: ImageApiService) {
+
+  }
 }
---
+++ Rubybooru.Angular/src/app/services/image-api/image-api.service.ts
@@ -0,0 +1,85 @@
+import {Injectable} from '@angular/core';
+import {RestApiService} from '../rest-api/rest-api.service';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+import {Image} from '../../entities/image';
+import {Tag} from "@angular/compiler/src/i18n/serializers/xml_helper";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageApiService extends RestApiService {
+
+  private readonly IMAGE_GET = 'image';
+
+  constructor(http: HttpClient) {
+    super(http);
+  }
+
+  public getImages(limit: number, offset: number = 0, tags: number[] = null): Observable<Image[]> {
+    const query = this.buildImageFilterQuery(limit, offset, tags);
+    return this.http.get<Image[]>(this.getImageUrl() + query)
+      .pipe(
+        catchError(this.handleError('getImages', []))
+      );
+  }
+
+  private buildImageFilterQuery(limit: number, offset: number, tags: number[]) {
+    let query = '?';
+
+    if (limit !== null) {
+      query += 'limit=' + limit + '&';
+    }
+
+    if (offset !== null) {
+      query += 'offset=' + offset + '&';
+    }
+
+    if (tags) {
+      tags.forEach(t => query += 'withTags=' + t + '&');
+    }
+
+    return query;
+  }
+
+  public getImage(id: number): Observable<Image> {
+    return this.http.get<Image>(this.getImageUrl(id)).pipe(
+      catchError(this.handleError<Image>('getImage(' + id + ')', null))
+    );
+  }
+
+  public getImageTags(id: number): Observable<Tag> {
+    return this.http.get<Tag>(this.getImageTagsUrl(id)).pipe(
+      catchError(this.handleError<Tag>('getImageTags(' + id + ')', null))
+    );
+  }
+
+  public getTags(ids: number[]): Observable<Map<number, Tag[]>> {
+    return this.http.get<Map<number, Tag[]>>(this.getTagsUrl(ids)).pipe(
+      catchError(this.handleError<Map<number, Tag[]>>('getTags(' + ids + ')', null))
+    );
+  }
+
+  public getImageFileUrl(id: number): string {
+    return this.getImageUrl(id) + '/file';
+  }
+
+  public getImagePreviewUrl(id: number, width: number, height: number, keepAspectRatio: boolean = true): string {
+    return this.getImageUrl(id) + '/preview?width=' + width + '&height=' + height + '&keepAspectRatio=' + keepAspectRatio
+  }
+
+  private getImageUrl(id: number = null): string {
+    return this.url + this.IMAGE_GET + (id !== null ? '/' + id : '');
+  }
+
+  private getImageTagsUrl(id: number): string {
+    return this.getImageUrl(id) + '/tag';
+  }
+
+  private getTagsUrl(ids: number[]): string {
+    let query = '';
+    ids.forEach(t => query += 'ids=' + t + '&');
+    return this.getImageUrl() + '/tags?' + query;
+  }
+}
