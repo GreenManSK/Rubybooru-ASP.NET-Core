3152059d9e59fcfb2280b376254417ffddb00dc8
Lukáš Kurčík <lukas.kurcik@gmail.com>
2020-06-14 22:44:11 +0200

    Full script for finding duplicate images

--- Rubybooru.Console/Runners/DuplicateCheck.cs
+++ Rubybooru.Console/Runners/DuplicateCheck.cs
@@ -19,17 +19,22 @@ namespace Rubybooru.Console.Runners
         private readonly IImagePreprocessorFactory _imagePreprocessorFactory;
         private readonly ImagePreprocessorUtils _imageUtils;
         private readonly IImageData _imageData;
+        private readonly IDuplicateRecordData _duplicateRecordData;
         private readonly IConfiguration _configuration;
         private readonly HashAlgorithm _hashAlgorithm;
 
         private readonly string _imagesPath;
         private readonly string _bwPath;
-        
+        private readonly int _maxRms;
+
+        private int counter;
+
         public DuplicateCheck(
             RubybooruDbContext db,
             IImagePreprocessorFactory imagePreprocessorFactory,
-            ImagePreprocessorUtils imageUtils, 
+            ImagePreprocessorUtils imageUtils,
             IImageData imageData,
+            IDuplicateRecordData duplicateRecordData,
             IConfiguration configuration,
             HashAlgorithm hashAlgorithm)
         {
@@ -37,29 +42,97 @@ namespace Rubybooru.Console.Runners
             _imagePreprocessorFactory = imagePreprocessorFactory;
             _imageUtils = imageUtils;
             _imageData = imageData;
+            _duplicateRecordData = duplicateRecordData;
             _configuration = configuration;
             _hashAlgorithm = hashAlgorithm;
 
             _imagesPath = _configuration.GetValue<string>("ImagesPath");
             _bwPath = _configuration.GetValue<string>("BlackWhitePath");
+            _maxRms = _configuration.GetValue<int>("DuplicateCheckerMaxRMS");
         }
 
         public int Run(DuplicateCheckOptions options)
         {
-            var uncheckedImages = from i in _db.Images where !i.DuplicateCheck select i;
+            System.Console.WriteLine("Creating bw images");
+            var uncheckedImages = from i in _db.Images where !i.DuplicateCheck orderby i.Id select i;
             CreateBwImages(uncheckedImages);
 
-            // Get all checked images
-            // Do the chekin'
-            // Set new images as checked
-            // Profit
-            
+            System.Console.WriteLine("Loading all bw images");
+            var allImages = (from i in _db.Images orderby i.Id select i).ToList();
+            var bwImages = LoadImages(allImages);
+
+            System.Console.WriteLine("Checking for duplicates");
+            foreach (var uncheckedImage in uncheckedImages)
+            {
+                CheckIfDuplicate(uncheckedImage, allImages, bwImages);
+                counter++;
+                if (counter % 100 == 0)
+                {
+                    System.Console.WriteLine($"Checked {counter} images");
+                    _duplicateRecordData.Commit();
+                }
+            }
+
+            System.Console.WriteLine("Saving results");
+            _duplicateRecordData.Commit();
+
             return 0;
+        }
+
+        private void CheckIfDuplicate(Image uncheckedImage, List<Image> allImages, Dictionary<int, int[][,]> bwImages)
+        {
+            var uncheckedBw = bwImages[uncheckedImage.Id];
+
+            foreach (var image in allImages)
+            {
+                if (image.Id == uncheckedImage.Id)
+                {
+                    break;
+                }
+
+                var imageBw = bwImages[image.Id];
+
+                if (IsDuplicate(uncheckedBw, imageBw))
+                {
+                    _duplicateRecordData.Add(new DuplicateRecord()
+                    {
+                        ImageAId = uncheckedImage.Id,
+                        ImageBId = image.Id
+                    });
+                }
+            }
+
+            uncheckedImage.DuplicateCheck = true;
+        }
+
+        private bool IsDuplicate(int[][,] uncheckedBw, int[][,] imageBw)
+        {
+            if (_imageUtils.MeanSquaredError(uncheckedBw[0], imageBw[0]) < _maxRms)
+                return true;
+            if (_imageUtils.MeanSquaredError(uncheckedBw[0], imageBw[1]) < _maxRms)
+                return true;
+            if (_imageUtils.MeanSquaredError(uncheckedBw[1], imageBw[0]) < _maxRms)
+                return true;
+            return false;
+        }
+
+        private Dictionary<int, int[][,]> LoadImages(IEnumerable<Image> images)
+        {
+            var result = new Dictionary<int, int[][,]>();
+            foreach (var image in images)
+            {
+                result.Add(image.Id, new[]
+                {
+                    _imageUtils.GetImageArray(GetFullBwPath(image)),
+                    _imageUtils.GetImageArray(GetFullBwPath(image, true))
+                });
+            }
+
+            return result;
         }
 
         private void CreateBwImages(IEnumerable<Image> uncheckedImages)
         {
-            System.Console.WriteLine("Creating bw images");
             foreach (var image in uncheckedImages)
             {
                 try
@@ -79,7 +152,8 @@ namespace Rubybooru.Console.Runners
                 }
                 catch (Exception e)
                 {
-                    System.Console.Error.WriteLine($"Error while creating bw version of {image.Path}/{image.Name}: {e}");
+                    System.Console.Error.WriteLine(
+                        $"Error while creating bw version of {image.Path}/{image.Name}: {e}");
                 }
             }
         }
