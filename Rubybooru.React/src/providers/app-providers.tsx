a5a3d1956bbba321c57ad12e8d319d1ec76af0b1
GreenManSK <lukas.kurcik@gmail.com>
2023-05-21 20:48:30 +0200

    Material UI

--- Rubybooru.React/src/providers/app-providers.tsx
+++ Rubybooru.React/src/providers/app-providers.tsx
@@ -1,11 +1,14 @@
 import React, { PropsWithChildren } from "react";
 import { HttpClientProvider } from "./http-client-provider";
 import { QueryClientProvider } from "./query-client-provider";
+import { ThemeProvider } from "./theme-provider";
 
 export const AppProviders: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <HttpClientProvider>
-      <QueryClientProvider>{children}</QueryClientProvider>
-    </HttpClientProvider>
+    <ThemeProvider>
+      <HttpClientProvider>
+        <QueryClientProvider>{children}</QueryClientProvider>
+      </HttpClientProvider>
+    </ThemeProvider>
   );
 };
---
+++ Rubybooru.React/src/providers/theme-provider.tsx
@@ -0,0 +1,43 @@
+import { CssBaseline } from "@mui/material";
+import {
+  ThemeOptions,
+  ThemeProvider as MaterialThemeProvider,
+  createTheme,
+} from "@mui/material/styles";
+import React, { PropsWithChildren } from "react";
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#fe728b",
+    },
+    secondary: {
+      main: "#973363",
+    },
+    text: {
+      primary: "#fe728b",
+      secondary: "rgba(254,114,139,0.7)",
+      disabled: "rgba(254,114,139,0.5)",
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          backgroundColor: "background.default",
+        },
+      },
+    },
+  },
+};
+
+export const ThemeProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  const theme = createTheme(themeOptions);
+  return (
+    <MaterialThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </MaterialThemeProvider>
+  );
+};
