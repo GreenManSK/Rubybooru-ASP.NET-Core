bda30b387237d3a777de47e28b3393d48b0f3859
GreenManSK <lukas.kurcik@gmail.com>
2023-05-23 19:41:03 +0200

    Stats under pagination

--- Rubybooru.React/src/components/image-list/pagination.tsx
+++ Rubybooru.React/src/components/image-list/pagination.tsx
@@ -11,14 +11,19 @@ const shuffleButtonStyles = {
   minWidth: 0,
 };
 
+const paginationStatsStyles = {
+  textAlign: "center",
+  fontSize: "1.2rem",
+};
 export interface IPaginationProps {
   page: number;
   pageCount: number;
   onPageChange: (page: number) => void;
+  items: number;
 }
 
 export const Pagination = (props: IPaginationProps) => {
-  const { page, pageCount, onPageChange } = props;
+  const { page, pageCount, onPageChange, items } = props;
 
   const setRandomPage = () => {
     const randomPage = Math.floor(Math.random() * (pageCount - 1) + 1);
@@ -29,25 +34,30 @@ export const Pagination = (props: IPaginationProps) => {
   };
 
   return (
-    <Box sx={paginationStyles}>
-      <MuiPagination
-        count={pageCount}
-        page={page}
-        onChange={(_, value) => onPageChange(value)}
-        shape="rounded"
-        color="primary"
-        variant="outlined"
-      />
-      {pageCount > 1 && (
-        <Button
+    <>
+      <Box sx={paginationStyles}>
+        <MuiPagination
+          count={pageCount}
+          page={page}
+          onChange={(_, value) => onPageChange(value)}
+          shape="rounded"
+          color="primary"
           variant="outlined"
-          size="small"
-          sx={shuffleButtonStyles}
-          onClick={setRandomPage}
-        >
-          <ShuffleIcon />
-        </Button>
-      )}
-    </Box>
+        />
+        {pageCount > 1 && (
+          <Button
+            variant="outlined"
+            size="small"
+            sx={shuffleButtonStyles}
+            onClick={setRandomPage}
+          >
+            <ShuffleIcon />
+          </Button>
+        )}
+      </Box>
+      {items !== undefined ? (
+        <Box sx={paginationStatsStyles}>({items} images)</Box>
+      ) : null}
+    </>
   );
 };
