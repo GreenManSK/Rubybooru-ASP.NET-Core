da3c65ee7865c60ef63b7c5adb2a6c88c33bbf73
GreenManSK <lukas.kurcik@gmail.com>
2025-03-04 11:04:54 +0100

    Fix random image

--- Rubybooru.Data/Sql/SqlImageData.cs
+++ Rubybooru.Data/Sql/SqlImageData.cs
@@ -183,7 +183,7 @@ namespace Rubybooru.Data.Sql
 
         public int GetRandomId()
         {
-            var image = _db.Images.FromSqlRaw("SELECT * FROM Images ORDER BY RAND() LIMIT 1").FirstOrDefault();
+            var image = _db.Images.FromSqlRaw("SELECT * FROM Images ORDER BY RANDOM() LIMIT 1").FirstOrDefault();
             return image?.Id ?? 0;
         }
 
